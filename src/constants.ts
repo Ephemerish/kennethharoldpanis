380e5896cb48a1086bb3320cd54837f52dd90f44
Ephemerish <kennethpanis24@gmail.com>
2025-08-17 16:44:21 +0800

    refactor: rename Article references to Blog for consistency and clarity; remove MostRecentArticle component and create MostRecentBlog component

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION =
-  "Articles, stories and tutorials from Tech People";
-export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
+  "Blogs, stories and tutorials from Tech People";
+export const HOMEPAGE_BLOG_LIMIT = 6;
+export const BLOGS_PER_PAGE = 6;
 export const PROJECTS_PER_PAGE = 9;
 export const HOMEPAGE_PROJECTS_LIMIT = 3;
