fecda9e8616a207029eb71147936f1bad95b4ac2
Ephemerish <kennethpanis24@gmail.com>
2026-07-22 10:24:19 +0800

    refactor: remove BioSubnav component and integrate resume data handling

---
+++ src/data/resume.ts
@@ -0,0 +1,3 @@
+// Drop the PDF at public/resume/kenneth-harold-panis-resume.pdf when ready, then flip this to true.
+export const resumeAvailable = false;
+export const resumePdfPath = "/resume/kenneth-harold-panis-resume.pdf";
